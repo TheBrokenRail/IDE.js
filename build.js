12c75fc40dcac564d51cfee9524272f9f1362706
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-03-07 17:41:34 -0500

    Update build.js

--- build.js
+++ build.js
@@ -1,1 +1,13 @@
+var nwBuilder = require('nw-builder');
+var nw = new nwBuilder({
+  files: './**/**',
+  platforms: ['win', 'linux', 'osx'],
+  flavor: 'normal',
+  appName: 'IDE.js'
+});
 
+nw.build().then(function () {
+  console.log('Build Successful!');
+}).catch(function (error) {
+  console.error(error);
+});

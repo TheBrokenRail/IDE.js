194b25dd09f96ed9615b72f04c6d93f51afedd5c
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-03-07 20:23:45 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
 cd build
+dir
 zip -r ../gh-pages/win64.zip win64
 zip -r ../gh-pages/win32.zip win32
 zip -r ../gh-pages/linux64.zip linux64
@@ -14,4 +15,4 @@ cd gh-pages
 git init
 git add .
 git commit --quiet -m "Deploy to Github Pages"
-git push --force "https://${GITHUB_TOKEN}@github.com/TheBrokenRail/IDE.js.git" master:gh-pages
+git push --force "https://${GITHUB_TOKEN}@github.com/TheBrokenRail/IDE.js.git" gh-pages

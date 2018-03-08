47a377a366ae52eaa717a4cbfd4d8bcd37412845
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-03-07 20:16:28 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,14 @@ git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
 cd build
+zip -r ../gh-pages/win64.zip win64
+zip -r ../gh-pages/win32.zip win32
+zip -r ../gh-pages/linux64.zip linux64
+zip -r ../gh-pages/linux32.zip linux32
+zip -r ../gh-pages/osx64.zip osx64
+cd ../
+mkdir gh-pages
+cd gh-pages
 git init
 git add .
 git commit --quiet -m "Deploy to Github Pages"

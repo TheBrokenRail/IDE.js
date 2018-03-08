17dd74d0c5166a3f5e99f11c481cbc29b0d24fd9
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-03-07 20:45:58 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,11 @@ git config --global user.name "Travis CI"
 mkdir gh-pages
 cd build
 dir
-zip -r ../gh-pages/IDE.js.zip ./IDE.js/
+zip -r ../gh-pages/win64.zip ./IDE.js/win64
+zip -r ../gh-pages/win32.zip ./IDE.js/win32
+zip -r ../gh-pages/linux64.zip ./IDE.js/linux64
+zip -r ../gh-pages/linux32.zip ./IDE.js/linux32
+zip -r ../gh-pages/osx64.zip ./IDE.js/osx64
 cd ../
 cd gh-pages
 git init
